180b759348d3980f37fb4a26156354dfa9614c15
Pravin Kumar <78309506+sahil2444@users.noreply.github.com>
2024-07-16 16:45:51 +0530

    Add files via upload

---
+++ all  C  projects/basic_ccalculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>      //making a tic tac toe game
+#include <stdlib.h>     //play against computer
+#include <string.h>
+#include <math.h>
+
+int main(){
+    double num1,num2,answer;
+    char op;
+    printf("this is a simple 2 by 2 calculator\n");
+start:
+    printf("\nEnter num1: ");
+    scanf("%lf",&num1);
+    printf("\nEnter an operator(+,-,*,/): ");
+    scanf(" %c",&op);
+    printf("\nEnter num2: ");
+    scanf("%lf",&num2);
+
+    switch(op){
+        case '+':
+        answer=num1+num2;
+        printf("\nresult: %.2lf \n",answer);
+        break;
+        case '-':
+        answer=num1-num2;
+        printf("\nresult: %.2lf \n",answer);
+        break;
+        case '*':
+        answer=num1*num2;
+        printf("\nresult: %.2lf \n",answer);
+        break;
+        case '/':
+        answer=num1/num2;
+        printf("\nresult: %.2lf \n",answer);
+        break;
+    }
+    goto start;
+
+    return 0;
+}

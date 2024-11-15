cac7e5e8e976f300defcc616d929b34cc2ea4c29
Pravin Kumar <78309506+sahil2444@users.noreply.github.com>
2024-11-15 21:34:27 +0530

    Update sqrAdd.c

--- all  C  projects/sqrAdd.c
+++ all  C  projects/sqrAdd.c
@@ -1,1 +1,23 @@
+#include <stdio.h>  //using recursion to add squares of numbers
+#include <stdlib.h>
+void sqrAdd(int num1,int num2,int *result);
+int main(){start:
+  int num1,num2,result=0;
+  printf("Enter num1: ");
+  scanf("%d",&num1);
+  printf("Enter num2: ");
+  scanf("%d",&num2);
 
+  sqrAdd(num1,num2,&result);
+  printf("sum of the squares of %d to %d is: %d\n\n",num1,num2,result);
+ goto start; 
+  return 0;
+}
+void sqrAdd(int num1,int num2,int *result)
+{
+  if(num1>num2){
+    return;
+  }
+  *result += num1*num1;
+  sqrAdd(num1+1,num2,result);
+}

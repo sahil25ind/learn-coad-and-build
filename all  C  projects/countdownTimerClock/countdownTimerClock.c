adc95a1cf31811a2a409d31f43877c2c7652bbf2
Pravin Kumar <78309506+sahil2444@users.noreply.github.com>
2024-12-30 23:56:56 +0530

    Add files via upload

---
+++ all  C  projects/countdownTimerClock/countdownTimerClock.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+    //simple countdown timer clock
+int main(){
+    int h,m,s;
+    int inputLimit = 99;
+    printf("***** Timer Clock *****\n");
+//input limit is 99
+    do{
+        printf("Enter Hour: ");
+        scanf("%d",&h);
+    }while(h > inputLimit || h < 0);
+
+    do{
+        printf("Enter Minutes: ");
+        scanf("%d",&m);
+    }while(m > inputLimit || m < 0);
+
+    do{
+        printf("Enter Second: ");
+        scanf("%d",&s);
+    }while(s > inputLimit || s < 0);
+
+    h = h * 3600;
+    m = m * 60;
+    s = s + h + m;
+    //declaring a local veriabel not sure why here!. why not up there!
+    int ts;
+
+    for(s;s > 0;s--){
+
+        sleep(1);
+        h = (s / 3600);
+        m = (s / 60) % 60;
+        ts = s % 60;
+
+        printf("\n%02d:%02d:%02d",h,m,ts);
+    }
+    printf("\n\nTIME'S UP!\n");
+}
